04faa708783577a2608b632a1b817f4e22151da2
Alejandro Perez <38706594+zalexzperez@users.noreply.github.com>
2022-06-25 01:58:49 +0200

    Added debugging commands and fixed some bugs. 1- Once reverse gear was disengaged, if reengaged, program didn't go back to state 1. We force this transition on the ISR now. 2- If reverse gear was preselected before system loaded, then reverse gear wasn't detected. Fixed by checking reverse signal status on void setup.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <Arduino.h>
 #include <esp_now.h>
 #include <WiFi.h>
-//#include <debug.h> //? Is this necessary? 
 
 /* Definitions */
 
@@ -12,7 +11,6 @@
   #define DEBUG_PRINTDEC(x) Serial.print (x, DEC)
   #define DEBUG_PRINTHEX(x) Serial.print (x, HEX)
   #define DEBUG_SERIALBEGIN(X) Serial.begin(X)
-
 #else 
   #define DEBUGPRINT(X) // As nothing
   #define DEBUGPRINTLN(X) // As nothing
@@ -25,12 +23,12 @@
 // Variable definitions
 bool speed_required = 0; // Flag to be enabled when we need to receive speed data
 uint8_t car_speed = 0; // This variable will allow us to know whether the car is stopped or moving too fast, used in the camera logic.
-int SM_state = 0; // This'll record the current state in the state machine. Our initial state is state 0.
+volatile int SM_state = 1; // This'll record the current state in the state machine. Our initial state is state 1. //? Need to declare this as volatile?
 volatile bool reverseEngaged = 0; // Flag to toggle in the ISR, depending on the reverse gear status. //? Need to declare this as volatile?
 unsigned long time_car_stopped = 0; // Varible to record when the car stops after disengaging reverse gear
 unsigned long time_fcam_enabled = 0; // Records when the front camera was enabled
 unsigned long wait_after_stopped = 2000; // Controls how much time to keep back camera signal after reverse disengaged and car stop.
-unsigned long wait_after_fcam_enabled = 3000; // 
+unsigned long wait_after_fcam_enabled = 6000; // 
 
 
 // Pin definitions
@@ -46,7 +44,7 @@ void speed_request(); // Function to request or unrequest speed to the external 
 
 
 // Receiver MAC Address. //TODO In the real project, replace with the actual receiver MAC address
-uint8_t broadcastAddress[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+uint8_t broadcastAddress[] = {0x78, 0xE3, 0x6D, 0x18, 0xCC, 0x78};
 
 // Create a variable of type esp_now_peer_info_t to store information about the receiving peer.
 esp_now_peer_info_t peerInfo;
@@ -55,7 +53,7 @@ esp_now_peer_info_t peerInfo;
 
 
 void setup() {
-  
+
   // Initialize Serial Monitor
   DEBUG_SERIALBEGIN(250000);
   
@@ -68,12 +66,10 @@ void setup() {
     return;
   }
   
-  // Once ESPNow is successfully Init, we will register for recv CB to
-  // get recv packer info
+  // Once ESPNow is successfully Init, we will register for recv CB to get recv packer info
   esp_now_register_recv_cb(OnDataRecv);
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Trasnmitted packet
+  // Once ESPNow is successfully Init, we will register for Send CB to get the status of Trasnmitted packet
   esp_now_register_send_cb(OnDataSent);
 
   // Register receving peer
@@ -91,9 +87,26 @@ void setup() {
   pinMode(videoSW_CTRL, OUTPUT); // This output will allow us to select which video input of the two to select. [LOW == Input 1 == Front camera signal  | HIGH == Input 2 == Back camera signal]
   pinMode(optocoupler_CTRL, OUTPUT); // This will generate a 12V camera trigger signal for the head unit. [LOW == Trigger signal OFF. HIGH == Trigger signal ON]
   pinMode(reverseSignalpin, INPUT_PULLDOWN); // [LOW == Reverse disengaged. HIGH == Reverse engaged]
+  #ifdef DEBUG
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, LOW);
+  #endif
 
   // Call reverseEngaged() when interrupt pin goes through a change transition
 	attachInterrupt(digitalPinToInterrupt(reverseSignalpin), reverseSignal, CHANGE);
+  
+  // Trigger signal off initially.
+  digitalWrite(optocoupler_CTRL, LOW);
+
+  // Select front camera video source.
+  digitalWrite(videoSW_CTRL, LOW);
+
+  // If reverse signal changes before system loads, we'd miss the transition. The next block prevents that.
+  if (digitalRead(reverseSignalpin)){
+    reverseEngaged = 1;
+    SM_state=1;
+    DEBUGPRINTLN("Reverse gear preselected");
+  } 
 }
 
 void loop() {
@@ -102,6 +115,7 @@ void loop() {
 
     case 0:{ // RESET STATE 
       
+      DEBUGPRINTLN("State = 0");
       // Disable trigger signal
       digitalWrite(optocoupler_CTRL, LOW);
 
@@ -112,14 +126,23 @@ void loop() {
       speed_required = 0;
       speed_request();
 
+      // Turn off built-in LED to visually inform of trigger signal status
+      #ifdef DEBUG
+      digitalWrite(BUILTIN_LED, LOW);
+      #endif
+
       // Move to the next state
       SM_state++;
+      DEBUGPRINT("State = ");
+      DEBUGPRINTLN(SM_state);
     } break;
   
     case 1:{  // WAIT FOR REVERSE ENGAGEMENT
       if (reverseEngaged){
         DEBUGPRINTLN("Reverse engaged");
         SM_state++;
+        DEBUGPRINT("State = ");
+        DEBUGPRINTLN(SM_state);
       }
     } break;
 
@@ -134,9 +157,17 @@ void loop() {
       // Request speed to external uC
       speed_required = 1;
       speed_request();
+      
+      #ifdef DEBUG
+        // Turn on built-in LED to visually inform of trigger signal status
+        digitalWrite(BUILTIN_LED, HIGH);
+      #endif
 
       // Move to the next state
       SM_state++;
+      DEBUGPRINT("State = ");
+      DEBUGPRINTLN(SM_state);
+      DEBUGPRINTLN("Waiting for disengagement");
     } break;
 
     case 3:{ // WAIT FOR DISENGAGEMENT
@@ -144,11 +175,14 @@ void loop() {
       if(!reverseEngaged){
         DEBUGPRINTLN("Reverse disengaged");
         SM_state++;
+        DEBUGPRINT("State = ");
+        DEBUGPRINTLN(SM_state);
+        DEBUGPRINTLN("Waiting for car to stop");
       }
     } break;
 
     case 4:{ // WAIT FOR CAR STOP
-
+ 
       if(car_speed == 0){
         DEBUGPRINTLN("Car supposedly stopped after reverse disengaged");
 
@@ -157,6 +191,9 @@ void loop() {
 
         // Move to the next state;
         SM_state++;
+        DEBUGPRINT("State = ");
+        DEBUGPRINTLN(SM_state);
+        DEBUGPRINTLN("Waiting some time after stopped");
       }
     } break;  
 
@@ -164,6 +201,9 @@ void loop() {
 
       if (millis()-time_car_stopped >= wait_after_stopped){
         SM_state++;
+        DEBUGPRINT("State = ");
+        DEBUGPRINTLN(SM_state);
+        DEBUGPRINTLN("Front camera will be enabled");
       }
     } break;
 
@@ -177,6 +217,9 @@ void loop() {
 
       // Move to the next state
       SM_state++;
+      DEBUGPRINT("State = ");
+      DEBUGPRINTLN(SM_state);
+      DEBUGPRINTLN("Wait for time or speed conditions to kill trigger signal");
     } break;
 
     case 7:{ // WAIT FOR TIME OR SPEED CONDITIONS
@@ -191,15 +234,23 @@ void loop() {
 void IRAM_ATTR reverseSignal(){
 
   // Reverse gear was engaged
-  if (digitalRead(reverseSignalpin))  reverseEngaged = 1;
+  if (digitalRead(reverseSignalpin)){
+    reverseEngaged = 1;
+    SM_state=1;
+    DEBUGPRINT("State = ");
+    DEBUGPRINTLN(SM_state);
+  } 
   //Reverse gear was disengaged:
-  else  reverseEngaged = 0;
+  else{
+    reverseEngaged = 0;
+  }  
 }
 
 void speed_request(){
 
   esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &speed_required, sizeof(speed_required));
-  if (result == ESP_OK) DEBUGPRINTLN("Sent with success");
+  if (result == ESP_OK) {//DEBUGPRINTLN("Sent with success");
+  }
   else  DEBUGPRINTLN("Error sending the data");
 }
 
@@ -207,8 +258,9 @@ void speed_request(){
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 
   memcpy(&car_speed, incomingData, sizeof(car_speed));
-  DEBUGPRINT("Bytes received: ");
-  DEBUGPRINTLN(len);
+  //DEBUGPRINT("Bytes received: ");
+  //DEBUGPRINTLN(len);
+  DEBUGPRINTLN("");
   DEBUGPRINT("Vehicle speed: ");
   DEBUGPRINTLN(car_speed);
 }
@@ -216,6 +268,6 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
 // Callback function that will executed when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 
-  DEBUGPRINT("\r\nLast Packet Send Status:\t");
-  DEBUGPRINTLN(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  //DEBUGPRINT("\r\nLast Packet Send Status:\t");
+  //DEBUGPRINTLN(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
